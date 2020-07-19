337cc83485712b09392ff95ce69ff901fe8f0c4e
saagarlearn <saagar.learn@gmail.com>
2020-07-19 17:14:10 +0100

    counter example

---
+++ springboot-counter/src/main/java/io/springboot/example/springbootcounter/SpringbootCounterApplication.java
@@ -0,0 +1,13 @@
+package io.springboot.example.springbootcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCounterApplication.class, args);
+	}
+
+}
---
+++ springboot-counter/src/test/java/io/springboot/example/springbootcounter/SpringbootCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springboot.example.springbootcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
